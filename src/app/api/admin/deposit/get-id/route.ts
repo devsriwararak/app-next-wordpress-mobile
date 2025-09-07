c9f03b972401ba6dd21fcc87b6361d1c11ec8206
DEV SRIWARARAK <devsriwararak.work@gmail.com>
2025-09-08 01:04:54 +0700

    add 7

--- src/app/api/admin/deposit/get-id/route.ts
+++ src/app/api/admin/deposit/get-id/route.ts
@@ -1,15 +1,17 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
+
+
 export async function POST(req: Request) {
   try {
-     const { product_id } = await req.json();
-
+    const { product_id } = await req.json();
     if (!product_id) {
-      return NextResponse.json({ error: "product_id is required" }, { status: 400 });
+      return NextResponse.json({ error: 'Invalid input' }, { status: 400 });
     }
+
 
     const deposit = await prisma.deposit_price.findUnique({
       where: { product_id: Number(product_id) },
@@ -18,11 +20,33 @@ export async function POST(req: Request) {
     if (!deposit) {
       return NextResponse.json({ error: "Data not found" }, { status: 404 });
     }
-
-    return NextResponse.json(deposit);
+    
+    return NextResponse.json(deposit, { status: 201 });
   } catch (error) {
-    console.error("API Error:", error);
-    return NextResponse.json({ error: "Failed to fetch data" }, { status: 500 });
+    return NextResponse.json({ error: 'Failed to create data' }, { status: 500 });
   }
 }
+
+// export async function POST(req: Request) {
+//   try {
+//      const { product_id } = await req.json();
+
+//     if (!product_id) {
+//       return NextResponse.json({ error: "product_id is required" }, { status: 400 });
+//     }
+
+//     const deposit = await prisma.deposit_price.findUnique({
+//       where: { product_id: Number(product_id) },
+//     });
+
+//     if (!deposit) {
+//       return NextResponse.json({ error: "Data not found" }, { status: 404 });
+//     }
+
+//     return NextResponse.json(deposit);
+//   } catch (error) {
+//     console.error("API Error:", error);
+//     return NextResponse.json({ error: "Failed to fetch data" }, { status: 500 });
+//   }
+// }
 
