3996d153de0547033979ecb12e3a22d7251b952f
DEV SRIWARARAK <devsriwararak.work@gmail.com>
2025-09-08 00:54:16 +0700

    add change admin 5

--- src/app/api/admin/deposit/get-id/route.ts
+++ src/app/api/admin/deposit/get-id/route.ts
@@ -7,19 +7,20 @@ export async function POST(request: NextRequest) {
   try {
     const body = await request.json();
     const product_id = body.product_id;
+
     if (!product_id) {
       return NextResponse.json({ error: "product_id is required" }, { status: 400 });
     }
 
-    const downData = await prisma.deposit_price.findUnique({
+    const deposit = await prisma.deposit_price.findUnique({
       where: { product_id: Number(product_id) },
     });
 
-    if (!downData) {
+    if (!deposit) {
       return NextResponse.json({ error: "Data not found" }, { status: 404 });
     }
 
-    return NextResponse.json(downData);
+    return NextResponse.json(deposit);
   } catch (error) {
     console.error("API Error:", error);
     return NextResponse.json({ error: "Failed to fetch data" }, { status: 500 });
